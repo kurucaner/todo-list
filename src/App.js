ef053acac7ed879d0c7fcf256c6274a066a698db
Caner <kurucaner@gmail.com>
2021-11-28 12:28:45 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import Todo from "./components/Todo";
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="Learn React"/>
-      <Todo text="Master at React"/>
-      <Todo text="Grandmaster at React"/>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewMeetupPage = () => {
+    return (
+        <div>
+            New Meetup Page
+        </div>
+    )
+}
+
+export default NewMeetupPage
