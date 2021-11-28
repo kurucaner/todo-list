8c660e6983502472f2deee4fec57a4dc4eee8e54
Caner <kurucaner@gmail.com>
2021-11-28 13:04:09 -0500

    mapped data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "React Meetup",
+    description: "React Meetup",
+    image:
+      "https://images.unsplash.com/photo-1589571208531-b9f8f9f8f9f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  },
+  {
+    id: "m2",
+    title: "React Native Meetup",
+    description: "React Native Meetup",
+    image:
+      "https://images.unsplash.com/photo-1589571208531-b9f8f9f8f9f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+  },
+];
+
 const AllMeetupsPage = () => {
-  return <div>All Meetups Page</div>;
+  return (
+    <div>
+      <h1>All Meetups Page</h1>
+      {DUMMY_DATA.map((meetup) => (
+        <div key={meetup.id}>
+          <h2>{meetup.title}</h2>
+          <p>{meetup.description}</p>
+          <img src={meetup.image} alt={meetup.title} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default AllMeetupsPage;
